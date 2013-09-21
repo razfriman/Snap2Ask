184f8cbe09c3ee2a132e048ada93017dcff79dd9
Raz Friman <superraz375@yahoo.com>
2013-09-21 11:19:44 -0700

    Update SQL File

--- snap2ask.sql
+++ snap2ask.sql
@@ -1,124 +1,197 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+CREATE DATABASE  IF NOT EXISTS `snap2ask` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `snap2ask`;
+-- MySQL dump 10.13  Distrib 5.5.32, for debian-linux-gnu (x86_64)
+--
+-- Host: 127.0.0.1    Database: snap2ask
+-- ------------------------------------------------------
+-- Server version	5.5.32-0ubuntu0.12.04.1
 
-CREATE SCHEMA IF NOT EXISTS `snap2ask` DEFAULT CHARACTER SET latin1 ;
-USE `snap2ask` ;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Table `snap2ask`.`answers`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `snap2ask`.`answers` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `question_id` INT(11) NOT NULL,
-  `tutor_id` INT(11) NOT NULL,
-  `text` VARCHAR(512) NOT NULL,
-  `rating` SMALLINT(6) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+--
+-- Table structure for table `answers`
+--
 
+DROP TABLE IF EXISTS `answers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `answers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `question_id` int(11) NOT NULL,
+  `tutor_id` int(11) NOT NULL,
+  `text` varchar(512) NOT NULL,
+  `rating` smallint(6) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `snap2ask`.`authentication_modes`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `snap2ask`.`authentication_modes` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 4
-DEFAULT CHARACTER SET = latin1;
+--
+-- Dumping data for table `answers`
+--
 
+LOCK TABLES `answers` WRITE;
+/*!40000 ALTER TABLE `answers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `answers` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- -----------------------------------------------------
--- Table `snap2ask`.`categories`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `snap2ask`.`categories` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(50) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+--
+-- Table structure for table `authentication_modes`
+--
 
+DROP TABLE IF EXISTS `authentication_modes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `authentication_modes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `snap2ask`.`images`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `snap2ask`.`images` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `data` BLOB NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+--
+-- Dumping data for table `authentication_modes`
+--
 
+LOCK TABLES `authentication_modes` WRITE;
+/*!40000 ALTER TABLE `authentication_modes` DISABLE KEYS */;
+INSERT INTO `authentication_modes` VALUES (1,'custom'),(2,'facebook'),(3,'google');
+/*!40000 ALTER TABLE `authentication_modes` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- -----------------------------------------------------
--- Table `snap2ask`.`users`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `snap2ask`.`users` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(100) NOT NULL,
-  `name` VARCHAR(100) NOT NULL,
-  `email` VARCHAR(100) NOT NULL,
-  `password` VARCHAR(100) NOT NULL,
-  `salt` VARCHAR(100) NOT NULL,
-  `balance` INT(11) NOT NULL DEFAULT '10',
-  `is_tutor` TINYINT(4) NOT NULL DEFAULT '0',
-  `is_admin` TINYINT(4) NOT NULL DEFAULT '0',
-  `authentication_mode_id` INT(11) NOT NULL,
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (1,'Math'),(2,'Science');
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `images`
+--
+
+DROP TABLE IF EXISTS `images`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `images` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `data` blob NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `images`
+--
+
+LOCK TABLES `images` WRITE;
+/*!40000 ALTER TABLE `images` DISABLE KEYS */;
+INSERT INTO `images` VALUES (1,'1234567');
+/*!40000 ALTER TABLE `images` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `questions`
+--
+
+DROP TABLE IF EXISTS `questions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `questions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `student_id` int(11) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `category_id` int(11) NOT NULL,
+  `status` varchar(50) NOT NULL DEFAULT 'Unanswered',
+  `image_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC),
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC),
-  INDEX `fk_users_authentication_modes1_idx` (`authentication_mode_id` ASC),
-  CONSTRAINT `fk_users_authentication_modes1`
-    FOREIGN KEY (`authentication_mode_id`)
-    REFERENCES `snap2ask`.`authentication_modes` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+  KEY `fk_questions_categories_idx` (`category_id`),
+  KEY `fk_questions_answers1_idx` (`status`),
+  KEY `fk_questions_images1_idx` (`image_id`),
+  KEY `fk_questions_users1_idx` (`student_id`),
+  CONSTRAINT `fk_questions_categories` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_questions_images1` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_questions_users1` FOREIGN KEY (`student_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `questions`
+--
 
--- -----------------------------------------------------
--- Table `snap2ask`.`questions`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `snap2ask`.`questions` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `student_id` INT(11) NOT NULL,
-  `desc` VARCHAR(255) NULL DEFAULT NULL,
-  `categories_id` INT(11) NOT NULL,
-  `answers_id` INT(11) NULL,
-  `images_id` INT(11) NOT NULL,
+LOCK TABLES `questions` WRITE;
+/*!40000 ALTER TABLE `questions` DISABLE KEYS */;
+INSERT INTO `questions` VALUES (12,10,'what is x+10=3',1,'Unanswered',1),(22,10,'What is the square root of 4 ?',1,'Unanswered',1),(23,10,'What is the square root of 4 ?',1,'Unanswered',1),(24,10,'What is the square root of 4 ?',1,'Unanswered',1);
+/*!40000 ALTER TABLE `questions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `salt` varchar(100) NOT NULL,
+  `balance` int(11) NOT NULL DEFAULT '10',
+  `is_tutor` tinyint(4) NOT NULL DEFAULT '0',
+  `is_admin` tinyint(4) NOT NULL DEFAULT '0',
+  `authentication_mode_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_questions_categories_idx` (`categories_id` ASC),
-  INDEX `fk_questions_answers1_idx` (`answers_id` ASC),
-  INDEX `fk_questions_images1_idx` (`images_id` ASC),
-  INDEX `fk_questions_users1_idx` (`student_id` ASC),
-  CONSTRAINT `fk_questions_categories`
-    FOREIGN KEY (`categories_id`)
-    REFERENCES `snap2ask`.`categories` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_questions_answers1`
-    FOREIGN KEY (`answers_id`)
-    REFERENCES `snap2ask`.`answers` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_questions_images1`
-    FOREIGN KEY (`images_id`)
-    REFERENCES `snap2ask`.`images` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_questions_users1`
-    FOREIGN KEY (`student_id`)
-    REFERENCES `snap2ask`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+  UNIQUE KEY `username_UNIQUE` (`username`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  KEY `fk_users_authentication_modes1_idx` (`authentication_mode_id`),
+  CONSTRAINT `fk_users_authentication_modes1` FOREIGN KEY (`authentication_mode_id`) REFERENCES `authentication_modes` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `users`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (10,'superraz','email','$2a$05$jQ36FhMZnEAdmTelg3h5k.OvIAfWOmF7mPuuWJRctzBNhkAGCNcvK','jQ36FhMZnEAdmTelg3h5k',40,0,0,1),(15,'{','{','$2a$05$h8dHvDd9VVWwV4OjqU6eM.TpLA4n1cDAP0E4mi8yJvL2HclXhWxgy','h8dHvDd9VVWwV4OjqU6eM',40,0,0,1),(27,'aaa','bbb','$2a$05$Mw0kYUTROvddIU1diS77X.VMhQhZ.a.CTMp/3xINdhDFUgmMAmfX6','Mw0kYUTROvddIU1diS77X',40,0,0,1),(28,'aaaa','bbbb','$2a$05$9jJrh8kWy9Ulp142YP3xi.VFbTey5MztiUCfoRQTaXNUBJOfzAbbW','9jJrh8kWy9Ulp142YP3xi',40,0,0,1),(31,'aaaaa','bbbbb','$2a$05$eajqP4wwhKar5DVNBwqnx.uyEmnnzsXrZqelTA9WhKnIHTBGNDK4C','eajqP4wwhKar5DVNBwqnx',40,0,0,1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-09-21 11:18:46
