1f6f2e91a5016783fe6293989ab3d8c897b221db
elenavillamil <mvillamilrod@smu.edu>
2013-10-07 00:24:23 -0500

    getAnswer is working.When using the function that produces json it set everything to null

--- Code/TestCode/insertAnswers.php
+++ Code/TestCode/insertAnswers.php
@@ -1,43 +1,32 @@
 <?php 
-	echo "<p>Stablishing connection to database <br/></p>";
 
-	$connection = mysql_connect("localhost", "cProject", "snap2ask");
-	if (!$connection)
+//eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee//
+//                                                                      //
+//      This script inserts an answer in the database          	        //
+//      It doesn't return anything                                      //
+//                                                                      //
+//eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee// 
+
+	$dbConnection = mysql_connect("localhost", "cProject", "snap2ask");
+	if (!$dbConnection)
 	{
 		die('Connection failure: ' . mysql_error());
 	}
-	echo "<p>Conection stablished sucessfully <br/><p>";
-		
+	
 	$questionID = $_POST['questionID'];
 	$tutorID = $_POST['tutorID'];
 	$text = $_POST['text'];
 	$date = $_SERVER['RESQUEST_TIME'];
 
-	//calling the function to insert the a new answer in the database
-	insertAnswer($connection, $questionID, $tutorID, $text, $date);
+	mysql_select_db("snap2ask", $dbConnection) or die ("It couldn't select snap2ask database. Error: " . mysql_error());
 
-	echo "<p>The question was succesfully inserted<br/><p>";
-
-	//closing the db connection
-	mysql_close($connection);
-
-	
-//eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee//
-//									//
-//	This fucntion inserts an answer in the database			//
-//	It doesn't return anything					//
-//									//
-//eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee//									
-	function insertAnswer($dbConnection, $questionID, $tutorID, $text, $date)
-	{
-		mysql_select_db("snap2ask", $dbConnection) or die ("It couldn't select snap2ask database. Error: " . mysql_error());
-
-		$insertAnswer = "INSERT INTO answers(question_id, tutor_id, text, date_created) VALUES ({$questionID}, {$tutorID}, '{$text}', '{$date}');";
+	$insertAnswer = "INSERT INTO answers(question_id, tutor_id, text, date_created) VALUES ({$questionID}, {$tutorID}, '{$text}', '{$date}');";
 		
-		if (!mysql_query($insertAnswer))
-		{
-			die("Impossible ot insert Answer" . mysql_error());
-		}
+	if (!mysql_query($insertAnswer))
+	{
+		die("Impossible ot insert Answer" . mysql_error());
 	}
+	echo "answer succesfully upload to the database";	
+	mysql_close($dbConnection);
 
 ?> 
