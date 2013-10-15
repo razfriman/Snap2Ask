6918376be0b2870b0a63f9c36e9c0196c4f5d627
Raymondd <rcmartin@smu.edu>
2013-10-15 18:01:55 -0500

    updated creatuserAccount.php though it still needs the 'salt'

--- Code/web/api/createUserAccount.php
+++ Code/web/api/createUserAccount.php
@@ -1,6 +1,41 @@
 <?
 // a function to create a new Account in the Database
 
+	$host = "localhost";
+	$username = "cProject";
+	$password = "snap2ask";
+
+	$dbConnection = mysql_connect($host, $username, $password);
+	if (!$dbConnection)
+	{
+		die('Connection failure: ' . mysql_error());
+	}
+
+
+	mysql_select_db("snap2ask", $connection) or die("It could select snap2ask database. Error: " . mysql_error());
+
+	$fname = $_POST['first_name'];
+	$lname = $_POST['last_name'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$date = time();
+	$date =date("Y-m-d H:i:s");
+
+
+	$insertUser = "INSERT INTO users(first_name, last_name, email, password, data_created) VALUES ({$fname}, {$lname}, '{$email}', '{$password}', '{$date}');";
+
+	
+	if (!mysql_query($insertUser))
+	{
+		die("Cannot Insert User" . mysql_error());
+	}
+
+	mysql_close($dbConnection);
+	
+
+	
+
+
 
 
 ?>
