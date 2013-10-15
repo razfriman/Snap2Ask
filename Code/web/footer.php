e1fb67cc8a2be9f83b1238fedc77bf4b82296278
Vipul Kohli <vkohli678@yahoo.com>
2013-10-15 01:23:45 -0500

    Added footer.php & Logged improvements in Feedback Forum in Google Docs

--- Code/web/footer.php
+++ Code/web/footer.php
@@ -1,5 +1,8 @@
-<div id="footertext">
+<footer>
+<div id="groupname">
 Snap-2-Ask Group, 2013
-
+</div>
+<div id = "bottommenu">
 Home | Tutor | Student | Suggestion Box | Contat Us
 </div>
+	</footer>
