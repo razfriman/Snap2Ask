173e47810b5670c4543dc283b2f3c790f4ca3ee2
superraz375 <superraz375@yahoo.com>
2013-10-18 07:01:33 -0500

    Work on browseQuestions.js

--- Code/web/js/browseQuestions.js
+++ Code/web/js/browseQuestions.js
@@ -7,13 +7,42 @@ function loadAllQuestions() {
 	$('#browseNav li:nth-child(1)').addClass('selected');
 	
 	// LOAD ALL QUESTIONS
-	$('#mainContent p').text("ALL QUESTIONS");
+	
+	$('#mainContent').empty();
+	
+	for (var i = 0; i < 50; i++) {
+		var item = document.createElement('div');
+		$(item).addClass('questionItem');
+		
+		var image = document.createElement('img');
+		$(image).addClass('questionImage');
+		image.setAttribute('src', 'http://placebox.es/150');
+	
+		var categoryLabel = document.createElement('label');
+		$(categoryLabel).text('Category: Math');
+		
+		var subcategoryLabel = document.createElement('label');
+		$(subcategoryLabel).text('Subategory: Geometry');
+		
+		var dateLabel = document.createElement('label');
+		$(dateLabel).text('Date: 01/05/2013 01:52:13 PM');
+		
+		item.appendChild(image);
+		item.appendChild(categoryLabel);
+		item.appendChild(subcategoryLabel);
+		item.appendChild(dateLabel);
+		
+		$('#mainContent')[0].appendChild(item);
+	
+	}
 }
 
 function loadCategoryQuestions() {
 	$('#browseNav li').removeClass('selected');
 	$('#browseNav li:nth-child(2)').addClass('selected');
 	
+	$('#mainContent').empty();
+
 	// LOAD QUESTIONS BY CATEGORY
 	$('#mainContent p').text("QUESTIONS BY CATEGORY");
 }
@@ -21,7 +50,9 @@ function loadCategoryQuestions() {
 function loadPreferredSubjectQuestions() {
 	$('#browseNav li').removeClass('selected');
 	$('#browseNav li:nth-child(3)').addClass('selected');
-	
+
+	$('#mainContent').empty();
+		
 	// LOAD PREFERRED SUBJECT QUESTIONS
 	$('#mainContent p').text("PREFERRED SUBJECT QUESTIONS");
 }
@@ -29,6 +60,8 @@ function loadPreferredSubjectQuestions() {
 function loadRecentQuestions() {
 	$('#browseNav li').removeClass('selected');
 	$('#browseNav li:nth-child(4)').addClass('selected');
+	
+	$('#mainContent').empty();
 	
 	// LOAD RECENT QUESTIONS
 	$('#mainContent p').text("RECENT QUESTIONS");
