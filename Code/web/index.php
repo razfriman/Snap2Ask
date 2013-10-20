6fe9185ec0fdb5557083ffc93e49e44b49baee27
superraz375 <superraz375@yahoo.com>
2013-10-20 13:53:25 -0500

    Fully implemented Google+ Sign-In.

--- Code/web/index.php
+++ Code/web/index.php
@@ -16,7 +16,48 @@ session_start();
 // Require the functions file
 require_once('functions.php');
 
+// 3rd Party Authentication
+if (isset($_POST['authentication_mode']))
+{
+	$request = array(
+			'last_name' => $_POST['last_name'],
+			'first_name' => $_POST['first_name'],
+			'email' => $_POST['email'],
+			'oauth_id' => $_POST['oauth_id'],
+			'password' => $_POST['token'],
+			'is_tutor' => true,
+			'register_or_login' => true,
+			'authentication_mode' => $_POST['authentication_mode'],
+			'preferred_category_id' => 1
+		);
 
+		//cURL used to collect login information
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $base_url . '/api/index.php/users');
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($ch, CURLOPT_HEADER, FALSE);
+		curl_setopt($ch, CURLOPT_POST, TRUE);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($request));
+		$response = curl_exec($ch);
+		curl_close($ch);
+	
+		
+		//sent to the be decoded
+		$responseObj = json_decode($response,true);
+
+		//depending on the response we either ask for different credentials or log the user in
+		if($responseObj['success'])
+		{
+			$_SESSION['user_id'] = $responseObj['user_id'];
+			$_SESSION['authentication_mode'] = $_POST['authentication_mode'];
+
+			// Stay logged in
+			setcookie('rememberCookie',true);
+		}
+}
+
+// Login / Register
 if (isset($_POST['submit'])) {
 
 	if($_POST['submit'] == 'Log in')
@@ -50,7 +91,8 @@ if (isset($_POST['submit'])) {
 		if($responseObj['success'])
 		{
 			$_SESSION['user_id'] = $responseObj['user_id'];
-
+			$_SESSION['authentication_mode'] = 'custom';
+			
 			// Stay logged in
 			setcookie('rememberCookie',true);
 		}
@@ -142,6 +184,83 @@ if (isset($_SESSION['user_id']))
 <head>
 	<title>Snap-2-Ask</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script type="text/javascript" src="js/jquery.redirect.min.js"> </script>
+    <script type="text/javascript">
+      (function() {
+       var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+       po.src = 'https://apis.google.com/js/client:plusone.js';
+       var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+     })();
+     
+          
+     // LOGOUT
+     //gapi.auth.signOut();
+     
+     function signinCallback(authResult) {
+     
+	  if (authResult['access_token']) {
+	
+		  // Update the app to reflect a signed in user
+		  // Hide the sign-in button now that the user is authorized, for example:
+		  document.getElementById('signinButton').setAttribute('style', 'display: none');
+
+		  gapi.auth.setToken(authResult);
+	  
+		  console.log('Sign-in state: SUCCESS');
+		  
+		  // Load Profile Info
+		  gapi.client.load('plus','v1', function(){	
+			  var request = gapi.client.plus.people.get({
+			   'userId': 'me'
+			 });
+			 request.execute(function(resp1) {
+			 
+			 var firstName  = resp1.name.familyName;
+			 var lastName = resp1.name.givenName;
+			 var oauthId = resp1.id;
+			 
+			 console.log('First Name: ' + resp1.name.familyName);
+			 console.log('Last Name: ' + resp1.name.givenName);
+			 console.log('OAuth ID: ' + resp1.id);
+			 
+			 // Load Email
+			  gapi.client.load('oauth2', 'v2', function() {
+				  gapi.client.oauth2.userinfo.get().execute(function(resp2) {
+				    
+				    var email = resp2.email;
+				    
+				    console.log('Email: ' + resp2.email);
+				    
+				    // REDIRECT				    
+			       var form = $('<form></form>');
+			   	   $(form).hide().attr('method','post').attr('action','index.php');
+			   	   $(form).append($('<input type="hidden" />').attr('name','token').val(authResult['access_token']));
+			   	   $(form).append($('<input type="hidden" />').attr('name','authentication_mode').val('google'));
+			   	   $(form).append($('<input type="hidden" />').attr('name','first_name').val(firstName));
+			   	   $(form).append($('<input type="hidden" />').attr('name','last_name').val(lastName));
+			   	   $(form).append($('<input type="hidden" />').attr('name','oauth_id').val(oauthId));
+			   	   $(form).append($('<input type="hidden" />').attr('name','email').val(email));
+			 	   $(form).appendTo('body').submit();
+				 	   
+				  })
+				});
+			 });
+			});
+       
+       }
+       else if (authResult['error']) {
+	    // Update the app to reflect a signed out user
+	    // Possible error values:
+	    //   "user_signed_out" - User is signed-out
+	    //   "access_denied" - User denied access to your app
+	    //   "immediate_failed" - Could not automatically log in the user
+	    console.log('Sign-in state: ' + authResult['error']);
+	  }
+	}
+    </script>
+    
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script src="js/validateInput.js" type="text/javascript"></script>
 	<script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script>
@@ -187,6 +306,20 @@ if(isset($_SESSION['msg']['login-err']))
 				<input type="password" name="password" placeholder="Password" title="Please enter a password" required autocomplete="on" />
 				<input  class="button" type="submit" name="submit" value="Log in" />
 			</form>
+			
+			<!-- Google+ Signin Button -->
+			<span id="signinButton">
+			  <span
+			    class="g-signin"
+			    data-callback="signinCallback"
+			    data-clientid="324181753300-40vh714rcp23n69pqu9mi2c2v605cj86.apps.googleusercontent.com"
+			    data-cookiepolicy="single_host_origin"
+			    data-requestvisibleactions="http://schemas.google.com/AddActivity"
+			    data-scope="https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email">
+			  </span>
+			</span>
+			
+			
 		</div>
 
 
--- Code/web/logout.php
+++ Code/web/logout.php
@@ -6,20 +6,59 @@ session_start();
 
 if(isset($_SESSION['user_id'])) {
 	
-	// Logout
+	$redirect_now = $_SESSION['authentication_mode'] == 'custom';
 	
 	// Clear the session
 	$_SESSION = array();
 	session_destroy();
 	
-	// Go to login page
-	header('Location: index.php');
-	exit;
+	if ($redirect_now) {
+		header('Location: index.php');
+	}
 } else {
-
-	// Go to login page
 	header('Location: index.php');
-	exit;
 }
 
 ?>
+
+<html>
+
+<head>
+	<title>Snap-2-Ask</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
+    <script type="text/javascript"> 
+     
+     function signinCallback(authResult) {
+     	// LOGOUT
+	 	gapi.auth.signOut();
+	 	
+	 	// Redirect to the main page
+	 	window.location.replace("index.php");
+     }
+     
+     window.setTimeout(function() {
+		 // Redirect to the main page
+		 window.location.replace("index.php");
+		 }, 5000);
+
+     
+     
+
+     
+     </script>
+</head>
+<body>
+Logging Out... <a href="index.php">Click here</a> if you are not redirected in up to 5 seconds.
+<span id="signinButton" style="display: none">
+			  <span
+			    class="g-signin"
+			    data-callback="signinCallback"
+			    data-clientid="324181753300-40vh714rcp23n69pqu9mi2c2v605cj86.apps.googleusercontent.com"
+			    data-cookiepolicy="single_host_origin"
+			    data-requestvisibleactions="http://schemas.google.com/AddActivity"
+			    data-scope="https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email">
+			  </span>
+			</span>
+</body>
+</html>
