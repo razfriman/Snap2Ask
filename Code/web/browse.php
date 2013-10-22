14d26eae6f91d5b8e60e67e5672220ff4382c66e
superraz375 <superraz375@yahoo.com>
2013-10-22 09:21:32 -0500

    Added favicon to all web pages

--- Code/web/browse.php
+++ Code/web/browse.php
@@ -21,9 +21,9 @@ if (!isset($_SESSION['user_id'])) {
 <head>
 	<title>Snap-2-Ask | Browse</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<link rel="shortcut icon" type="image/x-icon" href="res/favicon.ico">
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script src="js/browseQuestions.js" type="text/javascript"></script>
-	
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 
