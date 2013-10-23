7a040d9672559b9cfebb6164a22d2e0771096c18
Vipul Kohli <vkohli678@yahoo.com>
2013-10-22 20:52:34 -0500

    Create suggestionlist.php

---
+++ Code/web/suggestionlist.php
@@ -0,0 +1,18 @@
+<datalist id='suggestionlist'>
+
+<Graph of a function
+    Linear function
+    Secant
+    Slope
+    Tangent
+    Concave function
+    Finite difference
+    Radian
+    Factorial
+    Binomial theorem
+    Free variables and bound variables
+
+</datalist>
+
+
+<input type="submit" value="search"/>
