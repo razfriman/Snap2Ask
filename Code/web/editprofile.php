2e02d4a908217e72105247bba698e8055fab1e98
Vipul Kohli <vkohli678@yahoo.com>
2013-10-23 07:30:23 -0500

    editprofile.php UI ok, now need JS Validation & DB Query

--- Code/web/editprofile.php
+++ Code/web/editprofile.php
@@ -1,1 +1,77 @@
-<h1>COMING SOON!</h1>
+<?php
+
+// Start the named session
+session_name('loginSession');
+session_start();
+
+// Allow the included files to be executed
+define('inc_file', TRUE);
+
+
+if (!isset($_SESSION['user_id'])) {
+    // The user is not logged in
+    header('Location: index.php');
+	exit;
+}
+
+// Require the functions file
+require_once('functions.php');
+
+$responseObj = getUserInfo(true);
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Snap-2-Ask | Profile</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<link rel="shortcut icon" type="image/x-icon" href="res/favicon.ico">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+
+	<?php include('header.php') ?>
+
+
+    <div id="container">
+	
+		<div id="linksNav">
+			<ul>
+				<li><a href="browse.php" >Browse</a></li>
+				<li><a href="balance.php" >Balance</a></li>
+				<li class="selected" ><a href="profile.php" >Profile</a></li>
+			</ul>
+		</div>
+	
+		
+	
+		<div id="mainContent">
+            <h1>EDIT YOUR PROFILE</H1>
+			<!--POPULATE PROFILE INFORMATION HERE-->
+			<!--NEED TO VALIDATE EDITING PROFILE-->
+			
+<?php
+// Echo the information using sprintf
+// Escape special html characters to enhance XSS security
+echo sprintf("<label>First Name</label><input value='%s'>", htmlspecialchars($responseObj['first_name']));
+echo sprintf("<label>Last Name</label><input value='%s'>", htmlspecialchars($responseObj['last_name']));
+echo sprintf("<label>Email</label><input value='%s'>", htmlspecialchars($responseObj['email']));
+?>
+        <form name="edittutorprofile" id="edittutorprofile" action="#" method="post">
+		<input type="submit" value="Save Changes">
+		<a href="deact.php">Deactivate account</a>
+		</form>
+		</div>
+
+	</div>
+
+	<?php include('footer.php') ?>
+
+</body>
+</html>
