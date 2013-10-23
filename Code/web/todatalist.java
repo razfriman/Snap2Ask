ea33001b2704c10ef53d5b705e8a688158e5bffc
Vipul Kohli <vkohli678@yahoo.com>
2013-10-23 00:01:19 -0500

    code to convert courses to html datalist options

---
+++ Code/web/todatalist.java
@@ -0,0 +1,58 @@
+/**
+ * @(#)subjects.java
+ *
+ *
+ * @author 
+ * @version 1.00 2013/10/22
+ */
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+import java.net.*; 
+
+public class todatalist {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        	try{
+        	//url == some courses file to extract
+        	String url = "file:///C:/Users/user/Documents/javafiles/courses2.txt";
+       		String fileStr = "";
+            URL google = new URL(url);
+            URLConnection yc = google.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(yc
+                    .getInputStream()));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) 
+            {
+                fileStr = inputLine;
+                //String math = " in Science";
+                if (fileStr.length() < 1)
+                	continue;
+                //fileStr += math;
+                System.out.println("<option label=\"" + fileStr + "\" value=\"" + fileStr + "\">");
+            }
+        	
+        	
+        	
+        	
+        	
+        	
+        	
+        	
+        	
+        	
+        	
+        	
+        	
+        	
+        	}//end try
+        	catch (Exception e) 
+        {
+            e.printStackTrace();
+        }
+    }
+}
