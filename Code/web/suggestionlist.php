1b20f25844e33c77f48e5563676af81afab65a03
Vipul Kohli <vkohli678@yahoo.com>
2013-10-22 21:14:50 -0500

    Update suggestionlist.php

--- Code/web/suggestionlist.php
+++ Code/web/suggestionlist.php
@@ -1,16 +1,137 @@
 <datalist id='suggestionlist'>
+<option label="Graph of a function in Math" value="Graph of a function in Math">
+<option label="Linear function in Math" value="Linear function in Math">
+<option label="Secant in Math" value="Secant in Math">
+<option label="Slope in Math" value="Slope in Math">
+<option label="Tangent in Math" value="Tangent in Math">
+<option label="Concave function in Math" value="Concave function in Math">
+<option label="Finite difference in Math" value="Finite difference in Math">
+<option label="Radian in Math" value="Radian in Math">
+<option label="Factorial in Math" value="Factorial in Math">
+<option label="Binomial theorem in Math" value="Binomial theorem in Math">
+<option label="Free variables and bound variables in Math" value="Free variables and bound variables in Math">
+<option label="Limit (mathematics) in Math" value="Limit (mathematics) in Math">
+<option label="Limit of a function in Math" value="Limit of a function in Math">
+<option label="One-sided limit in Math" value="One-sided limit in Math">
+<option label="Limit of a sequence in Math" value="Limit of a sequence in Math">
+<option label="Indeterminate form in Math" value="Indeterminate form in Math">
+<option label="Orders of approximation in Math" value="Orders of approximation  in Math">
+<option label="Derivative in Math" value="Derivative in Math">
+<option label="Notation in Math" value="Notation in Math">
+<option label="Newton's notation for differentiation in Math" value="Newton's notation for differentiation in Math">
+<option label="Leibniz's notation for differentiation in Math" value="Leibniz's notation for differentiation in Math">
+<option label="Simplest rules in Math" value="Simplest rules in Math">
+<option label="Derivative of a constant in Math" value="Derivative of a constant in Math">
+<option label="Sum rule in differentiation in Math" value="Sum rule in differentiation in Math">
+<option label="Constant factor rule in differentiation in Math" value="Constant factor rule in differentiation in Math">
+<option label="Linearity of differentiation in Math" value="Linearity of differentiation in Math">
+<option label="Power rule in Math" value="Power rule in Math">
+<option label="Derivative (examples) in Math" value="Derivative (examples) in Math">
+<option label="Chain rule in Math" value="Chain rule in Math">
+<option label="Product rule in Math" value="Product rule in Math">
+<option label="Quotient rule in Math" value="Quotient rule in Math">
+<option label="Inverse functions and differentiation in Math" value="Inverse functions and differentiation in Math">
+<option label="Implicit differentiation in Math" value="Implicit differentiation in Math">
+<option label="Stationary point in Math" value="Stationary point in Math">
+<option label="Maxima and minima in Math" value="Maxima and minima in Math">
+<option label="First derivative test in Math" value="First derivative test in Math">
+<option label="Second derivative test in Math" value="Second derivative test in Math">
+<option label="Extreme value theorem in Math" value="Extreme value theorem in Math">
+<option label="Differential equation in Math" value="Differential equation in Math">
+<option label="Differential operator in Math" value="Differential operator in Math">
+<option label="Newton's method in Math" value="Newton's method in Math">
+<option label="Taylor's theorem in Math" value="Taylor's theorem in Math">
+<option label="L'Hôpital's rule in Math" value="L'Hôpital's rule in Math">
+<option label="Leibniz's rule in Math" value="Leibniz's rule in Math">
+<option label="Mean value theorem in Math" value="Mean value theorem in Math">
+<option label="Logarithmic derivative in Math" value="Logarithmic derivative in Math">
+<option label="Differential (calculus) in Math" value="Differential (calculus) in Math">
+<option label="Related rates in Math" value="Related rates in Math">
+<option label="Regiomontanus' angle maximization problem in Math" value="Regiomontanus' angle maximization problem in Math">
+<option label="Integral calculus in Math" value="Integral calculus in Math">
+<option label="Antiderivative, Indefinite integral in Math" value="Antiderivative, Indefinite integral in Math">
+<option label="Simplest rules in Math" value="Simplest rules in Math">
+<option label="Sum rule in integration in Math" value="Sum rule in integration in Math">
+<option label="Constant factor rule in integration in Math" value="Constant factor rule in integration in Math">
+<option label="Linearity of integration in Math" value="Linearity of integration in Math">
+<option label="Arbitrary constant of integration in Math" value="Arbitrary constant of integration in Math">
+<option label="Fundamental theorem of calculus in Math" value="Fundamental theorem of calculus in Math">
+<option label="Integration by parts in Math" value="Integration by parts in Math">
+<option label="Inverse chain rule method in Math" value="Inverse chain rule method in Math">
+<option label="Integration by substitution in Math" value="Integration by substitution in Math">
+<option label="Tangent half-angle substitution in Math" value="Tangent half-angle substitution in Math">
+<option label="Differentiation under the integral sign in Math" value="Differentiation under the integral sign in Math">
+<option label="Trigonometric substitution in Math" value="Trigonometric substitution in Math">
+<option label="Partial fractions in integration in Math" value="Partial fractions in integration in Math">
+<option label="Quadratic integral in Math" value="Quadratic integral in Math">
+<option label="Proof that 22/7 exceeds p in Math" value="Proof that 22/7 exceeds p in Math">
+<option label="Trapezium rule in Math" value="Trapezium rule in Math">
+<option label="Integral of the secant function in Math" value="Integral of the secant function in Math">
+<option label="Integral of secant cubed in Math" value="Integral of secant cubed in Math">
+<option label="Arclength in Math" value="Arclength in Math">
+<option label="Special functions and numbers in Math" value="Special functions and numbers in Math">
+<option label="Natural logarithm in Math" value="Natural logarithm in Math">
+<option label="e (mathematical constant) in Math" value="e (mathematical constant) in Math">
+<option label="Exponential function in Math" value="Exponential function in Math">
+<option label="Stirling's approximation in Math" value="Stirling's approximation in Math">
+<option label="Bernoulli numbers in Math" value="Bernoulli numbers in Math">
+<option label="Numerical integration in Math" value="Numerical integration in Math">
+<option label="See also list of numerical analysis topics in Math" value="See also list of numerical analysis topics in Math">
+<option label="Rectangle method in Math" value="Rectangle method in Math">
+<option label="Trapezium rule in Math" value="Trapezium rule in Math">
+<option label="Simpson's rule in Math" value="Simpson's rule in Math">
+<option label="Newton–Cotes formulas in Math" value="Newton–Cotes formulas in Math">
+<option label="Gaussian quadrature in Math" value="Gaussian quadrature in Math">
+<option label="Lists and tables in Math" value="Lists and tables in Math">
+<option label="Table of common limits in Math" value="Table of common limits in Math">
+<option label="Table of derivatives in Math" value="Table of derivatives in Math">
+<option label="Table of integrals in Math" value="Table of integrals in Math">
+<option label="Table of mathematical symbols in Math" value="Table of mathematical symbols in Math">
+<option label="List of integrals in Math" value="List of integrals in Math">
+<option label="List of integrals of rational functions in Math" value="List of integrals of rational functions in Math">
+<option label="List of integrals of irrational functions in Math" value="List of integrals of irrational functions in Math">
+<option label="List of integrals of trigonometric functions in Math" value="List of integrals of trigonometric functions in Math">
+<option label="List of integrals of inverse trigonometric functions in Math" value="List of integrals of inverse trigonometric functions in Math">
+<option label="List of integrals of hyperbolic functions in Math" value="List of integrals of hyperbolic functions in Math">
+<option label="List of integrals of exponential functions in Math" value="List of integrals of exponential functions in Math">
+<option label="List of integrals of logarithmic functions in Math" value="List of integrals of logarithmic functions in Math">
+<option label="List of integrals of area functions in Math" value="List of integrals of area functions in Math">
+<option label="Multivariable in Math" value="Multivariable in Math">
+<option label="Partial derivative in Math" value="Partial derivative in Math">
+<option label="Disk integration in Math" value="Disk integration in Math">
+<option label="Shell integration in Math" value="Shell integration in Math">
+<option label="Gabriel's horn in Math" value="Gabriel's horn in Math">
+<option label="Jacobian matrix in Math" value="Jacobian matrix in Math">
+<option label="Hessian matrix in Math" value="Hessian matrix in Math">
+<option label="Curvature in Math" value="Curvature in Math">
+<option label="Green's theorem in Math" value="Green's theorem in Math">
+<option label="Divergence theorem in Math" value="Divergence theorem in Math">
+<option label="Stokes' theorem in Math" value="Stokes' theorem in Math">
+<option label="Series in Math" value="Series in Math">
+<option label="Infinite series in Math" value="Infinite series in Math">
+<option label="Maclaurin series, Taylor series in Math" value="Maclaurin series, Taylor series in Math">
+<option label="Fourier series in Math" value="Fourier series in Math">
+<option label="Euler–Maclaurin formula in Math" value="Euler–Maclaurin formula in Math">
+<option label="History in Math" value="History in Math">
+<option label="Adequality in Math" value="Adequality in Math">
+<option label="Infinitesimal in Math" value="Infinitesimal in Math">
+<option label="Archimedes' use of infinitesimals in Math" value="Archimedes' use of infinitesimals in Math">
+<option label="Gottfried Leibniz in Math" value="Gottfried Leibniz in Math">
+<option label="Isaac Newton in Math" value="Isaac Newton in Math">
+<option label="Method of Fluxions in Math" value="Method of Fluxions in Math">
+<option label="Infinitesimal calculus in Math" value="Infinitesimal calculus in Math">
+<option label="Brook Taylor in Math" value="Brook Taylor in Math">
+<option label="Colin Maclaurin in Math" value="Colin Maclaurin in Math">
+<option label="Leonhard Euler in Math" value="Leonhard Euler in Math">
+<option label="Law of continuity in Math" value="Law of continuity in Math">
+<option label="History of calculus in Math" value="History of calculus in Math">
+<option label="Generality of algebra in Math" value="Generality of algebra in Math">
+<option label="Nonstandard calculus in Math" value="Nonstandard calculus in Math">
+<option label="Elementary Calculus: An Infinitesimal Approach in Math" value="Elementary Calculus: An Infinitesimal Approach in Math">
+<option label="Nonstandard calculus in Math" value="Nonstandard calculus in Math">
+<option label="Infinitesimal in Math" value="Infinitesimal in Math">
+<option label="Archimedes' use of infinitesimals in Math" value="Archimedes' use of infinitesimals in Math">
 
-<Graph of a function
-    Linear function
-    Secant
-    Slope
-    Tangent
-    Concave function
-    Finite difference
-    Radian
-    Factorial
-    Binomial theorem
-    Free variables and bound variables
 
 </datalist>
 
