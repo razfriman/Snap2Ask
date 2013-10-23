7d896ae17f1430755dc28aa3139b8a26ed820f1a
Vipul Kohli <vkohli678@yahoo.com>
2013-10-23 07:38:15 -0500

    View ISSUES! VERY IMPORTANT! See my previous commits!

--- Code/web/addfunds.php
+++ Code/web/addfunds.php
@@ -1,1 +1,147 @@
 <h1>COMING SOON!</h1>
+
+
+<?php
+
+// Start the named session
+session_name('loginSession');
+session_start();
+
+// Allow the included files to be executed
+define('inc_file', TRUE);
+
+if (!isset($_SESSION['user_id'])) {
+	// The user is not logged in
+	header('Location: index.php');
+	exit;
+}
+
+// Require the functions file
+require_once('functions.php');
+
+$responseObj = getUserInfo(true);
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Snap-2-Ask | Balance</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<link rel="shortcut icon" type="image/x-icon" href="res/favicon.ico">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+
+	<?php include('header.php') ?>
+	
+	<div id="container">
+
+		<div id="linksNav">
+			<ul>
+				<li><a href="browse.php" >Browse</a></li>
+				<li class="selected"><a href="balance.php" >Balance</a></li>
+				<li><a href="profile.php" >Profile</a></li>
+			</ul>
+		</div>
+	
+		
+	
+		<div id="mainContent">
+			<!--POPULATE BALANCE INFO HERE-->
+			<!--Validate input values-->
+<?php
+
+// Echo the information using sprintf
+// Escape special html characters to enhance XSS security
+echo sprintf("<label>Balance</label><input readonly='YES' value='%s'>", htmlspecialchars($responseObj['balance']));
+
+?>
+		<form name="addtutorfunds" id="addtutorfunds" action="#" method=<?php
+
+// Start the named session
+session_name('loginSession');
+session_start();
+
+// Allow the included files to be executed
+define('inc_file', TRUE);
+
+if (!isset($_SESSION['user_id'])) {
+    // The user is not logged in
+	header('Location: index.php');
+	exit;
+}
+
+// Require the functions file
+require_once('functions.php');
+
+$responseObj = getUserInfo(true);
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Snap-2-Ask | Balance</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<link rel="shortcut icon" type="image/x-icon" href="res/favicon.ico">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+
+	<?php include('header.php') ?>
+	
+	<div id="container">
+
+		<div id="linksNav">
+			<ul>
+				<li><a href="browse.php" >Browse</a></li>
+				<li class="selected"><a href="balance.php" >Balance</a></li>
+				<li><a href="profile.php" >Profile</a></li>
+			</ul>
+		</div>
+	
+		
+	
+		<div id="mainContent">
+			<!--POPULATE BALANCE INFO HERE-->
+			<!--Validate input values-->
+<?php
+
+// Echo the information using sprintf
+// Escape special html characters to enhance XSS security
+echo sprintf("<label>Balance</label><input readonly='YES' value='%s'>", htmlspecialchars($responseObj['balance']));
+
+?>
+		<form name="addtutorfunds" id="addtutorfunds" action="#" method="put">
+		<input type="submit" value="Deposit Funds">
+		</form>
+		<form name="withdrawfunds" id="withdrawfunds" action="#" method="put">
+		<input type="submit" value="Withdraw Funds">
+		</form>
+        
+        <section id="txnhistory">
+        <h3>Available Funds: $40.00</h3>
+        <h3>Pending Funds: $ 5.50</h3>
+        <a href="pending.php">Why are my funds pending?</a>
+    <h2><br/>COMING SOON:</h2> <p>Tutor's withdrawal and deposit history will be shown here in table format.</p>
+	</section>
+    
+		</div>
+	</div>
+	
+
+
+	<?php include('footer.php') ?>
+
+</body>
+</html>
+
