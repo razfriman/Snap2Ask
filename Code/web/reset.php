0cec69d874729b0be3c325f6895968b62c8f3397
Vipul Kohli <vkohli678@yahoo.com>
2013-10-24 02:42:51 -0500

    Update reset.php

--- Code/web/reset.php
+++ Code/web/reset.php
@@ -1,13 +1,12 @@
 <?php
 $pass = "";
-for ($x = 0; $x < 3; $x++){
+for ($x = 0; $x < 3; $x++)
     $pass .= chr(rand(65, 90)) . chr(rand(97, 122)) . chr(rand(48, 57));
-}
+    //pattern: upper,number,lower * 3
 $toUserEmail = "";
 $from = "service@snap2ask.com";
 $headers = "From:" . $from;
 $message = "Your new Snap2Ask password is: " . $pass;
 mail($toUserEmail,'',$message,$headers);
-echo "Mail Sent
-
+echo "Reset password e-mail Sent";
 ?>
