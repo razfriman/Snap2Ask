1272c98d829f4b9e2c6844f82662808e6c492966
elenavillamil <mvillamilrod@smu.edu>
2013-10-26 08:52:23 -0500

    working on sending email

--- Code/web/forgotPassword.php
+++ Code/web/forgotPassword.php
@@ -1,38 +1,43 @@
 <?php
-	$email = $_GET['email'];
-	//connect to databe
-	$dbConnection = mysql_connect("localhost", "cProject", "snap2ask");
-	if (!$dbConnection)
+$email = $_GET['email'];
+//$email = "mvillamilrod@smu.edu";
+//connect to databe
+$dbConnection = mysql_connect("localhost", "cProject", "snap2ask");
+if (!$dbConnection)
+{
+	die("Connection failure: " . mysql_error());
+}
+mysql_select_db("snap2ask", $dbConnection) or die ("It couldn'tslect snap2ask database. Error: " . mysql_error());
+
+$getUser = "SELECT * from users WHERE email = '{$email}';";
+$user = mysql_query($getUser);
+$myUser = mysql_fetch_assoc($user);
+//if email in our database 
+if ($myUser != NULL)
+{
+	//generate password
+	$password = rand(100000, 1000000);
+	$password = (string)$password;
+	//update new password in the db 
+	$resetPass = "UPDATE users set password = '{$password}' where id = {$myUser['id']};";
+	if (!mysql_query($resetPass))
 	{
-		die("Connection failure: " . mysql_error());
+		die ("Impossible to reset Password" . mysql_error());
 	}
-	mysql_select_db("snap2ask", $dbConnection) or die ("It couldn'tslect snap2ask database. Error: " . mysql_error());
-	
-	$getUser = "SELECT * from users WHERE email = '{$email}';";
-	$user = mysql_query($getUser);
-	$myUser = mysql_fetch_assoc($user);
-	//if email in our database 
-	if ($myUser != NULL)
-	{
-		//generate password
-		$password = rand(100000, 1000000);
-		$password = (string)$password;
-		//update new password in the db 
-		$resetPass = "UPDATE users set password = '{$password}' where id = {$myUser['id']};";
-		if (!mysql_query($resetPass))
-		{
-			die ("Impossible to reset Password" . mysql_error());
-		}
-		$subject = "Snap2ask Reset Password";
-		$message = "Your new password is: " . $password;
-		$headers = "From: snap2ask";
-		mail($to, $subject, $message, $headers); 
-		echo "An email has been sent with your new password";
-;
-	//email new password
+	$subject = "Snap2ask Reset Password";
+	$message = "Your new password is: " . $password;
+	//$headers = "From: ";	
+	if (mail($email, $subject, $message))
+	{ 
+		echo "An email has been sent to " . $email . " with your new password";
 	}
-	else 
-	{
-		echo "couldn't find email " . $email . "\n";
+	else{
+		echo"Impossible to send email to " . $email . "\r\n";
 	}
+//email new password
+}
+else 
+{
+	echo "couldn't find email " . $email . "\n";
+}
 ?>
