49adb9b02053b01dab8d33fc3a68ffb98aded3ab
Vipul Kohli <vkohli678@yahoo.com>
2013-10-26 11:55:16 -0500

    Update resetpass.php

--- Code/web/resetpass.php
+++ Code/web/resetpass.php
@@ -10,9 +10,6 @@
             for ($x = 0; $x < 3; $x++) 
                 $pass .= chr(rand(65, 90)) . chr(rand(97, 122)) . chr(rand(48, 57));
             //pattern: upper,lower,number,upper,lower,number,upper,lower,number 
-            $toUserEmail = ""; $from = "service@snap2ask.com"; 
-            $headers = "From:" . $from; 
-            $message = "Your new Snap2Ask password is: " . $pass; mail($toUserEmail,'',$message,$headers); 
             // Echo the information using sprintf 
             // Escape special html characters to enhance XSS security 
             echo sprintf("<label>Enter your E-mail Address</label><input type='text' name='toemail'>", htmlspecialchars($responseObj['first_name'])); 
