2998979b9cd5e8b2fdec7b758acb8e4943c83752
elenavillamil <mvillamilrod@smu.edu>
2013-10-31 16:45:38 -0500

    Raz is a genious

--- Code/sql/snap2ask.sql
+++ Code/sql/snap2ask.sql
@@ -51,7 +51,7 @@ CREATE TABLE `validationQuestions` (
   `optionB` varchar(150) NOT NULL,
   `optionC` varchar(150) NOT NULL,
   `rightAnswer` varchar(2) NOT NULL,
-  `categoryID` int(11) NOT NULL,validationQuestions
+  `categoryID` int(11) NOT NULL,
   PRIMARY KEY (`id`),	
   CONSTRAINT `fk_validationQuestions_1` FOREIGN KEY (`categoryID`) REFERENCES categories(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE = InnoDB AUTO_INCREMENT = 2 DEFAULT CHARSET = latin1;
