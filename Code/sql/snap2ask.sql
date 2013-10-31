1711d145d22dc8a10f9a5ac89b071bff4ddfb9bc
superraz375 <superraz375@yahoo.com>
2013-10-31 16:36:06 -0500

    Add a table for verified categories

--- Code/sql/snap2ask.sql
+++ Code/sql/snap2ask.sql
@@ -1,158 +1,180 @@
-CREATE DATABASE  IF NOT EXISTS `snap2ask` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `snap2ask`;
--- MySQL dump 10.13  Distrib 5.5.32, for debian-linux-gnu (x86_64)
---
--- Host: 127.0.0.1    Database: snap2ask
--- ------------------------------------------------------
--- Server version	5.5.32-0ubuntu0.12.04.1
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE SCHEMA IF NOT EXISTS `snap2ask` DEFAULT CHARACTER SET latin1 ;
+USE `snap2ask` ;
 
---
--- Table structure for table `answers`
---
+-- -----------------------------------------------------
+-- Table `snap2ask`.`categories`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `snap2ask`.`categories` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = latin1;
 
-DROP TABLE IF EXISTS `answers`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `answers` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `question_id` int(11) NOT NULL,
-  `tutor_id` int(11) NOT NULL,
-  `text` varchar(512) NOT NULL,
-  `rating` smallint(6) DEFAULT NULL,
-  `status` varchar(45) NOT NULL DEFAULT 'pending',
-  `date_created` datetime NOT NULL,
+
+-- -----------------------------------------------------
+-- Table `snap2ask`.`subcategories`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `snap2ask`.`subcategories` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `category_id` INT(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_answers_1_idx` (`question_id`),
-  CONSTRAINT `fk_answers_1` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `validationQuestions`
---
-
-DROP TABLE IF EXISTS `validationQuestions`;
-
-CREATE TABLE `validationQuestions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `question` varchar(200) NOT NULL,
-  `optionA` varchar(150) NOT NULL,
-  `optionB` varchar(150) NOT NULL,
-  `optionC` varchar(150) NOT NULL,
-  `rightAnswer` varchar(2) NOT NULL,
-  `categoryID` int(11) NOT NULL,validationQuestions
-  PRIMARY KEY (`id`),	
-  CONSTRAINT `fk_validationQuestions_1` FOREIGN KEY (`categoryID`) REFERENCES categories(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE = InnoDB AUTO_INCREMENT = 2 DEFAULT CHARSET = latin1;
+  INDEX `fk_subcategories_1_idx` (`category_id` ASC),
+  CONSTRAINT `fk_subcategories_1`
+    FOREIGN KEY (`category_id`)
+    REFERENCES `snap2ask`.`categories` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 7
+DEFAULT CHARACTER SET = latin1;
 
 
---
--- Table structure for table `categories`
---
-
-DROP TABLE IF EXISTS `categories`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `categories` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `questions`
---
-
-DROP TABLE IF EXISTS `questions`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `questions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `student_id` int(11) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  `category_id` int(11) NOT NULL,
-  `subcategory_id` int(11) NOT NULL,
-  `status` int(11) NOT NULL DEFAULT '0',
-  `times_answered` int(11) NOT NULL DEFAULT '0',
-  `image_url` varchar(255) NOT NULL,
-  `image_thumbnail_url` varchar(255) NOT NULL,
-  `date_created` datetime NOT NULL,
+-- -----------------------------------------------------
+-- Table `snap2ask`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `snap2ask`.`users` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(100) NOT NULL,
+  `oauth_id` VARCHAR(100) NULL,
+  `password` VARCHAR(100) NOT NULL,
+  `salt` VARCHAR(100) NOT NULL,
+  `balance` INT(11) NOT NULL DEFAULT '10',
+  `is_tutor` TINYINT(4) NOT NULL DEFAULT '0',
+  `is_admin` TINYINT(4) NOT NULL DEFAULT '0',
+  `date_created` DATETIME NOT NULL,
+  `authentication_mode` ENUM('custom','facebook','google') NOT NULL,
+  `first_name` VARCHAR(45) NOT NULL,
+  `last_name` VARCHAR(45) NOT NULL,
+  `rating` INT(11) NULL,
+  `password_reset_token` VARCHAR(50) NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_questions_categories_idx` (`category_id`),
-  KEY `fk_questions_answers1_idx` (`status`),
-  KEY `fk_questions_users1_idx` (`student_id`),
-  KEY `fk_questions_1_idx` (`subcategory_id`),
-  CONSTRAINT `fk_questions_1` FOREIGN KEY (`subcategory_id`) REFERENCES `subcategories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_questions_categories` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_questions_users1` FOREIGN KEY (`student_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC, `authentication_mode` ASC))
+ENGINE = InnoDB
+AUTO_INCREMENT = 55
+DEFAULT CHARACTER SET = latin1;
 
---
--- Table structure for table `subcategories`
---
 
-DROP TABLE IF EXISTS `subcategories`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `subcategories` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  `category_id` int(11) NOT NULL,
+-- -----------------------------------------------------
+-- Table `snap2ask`.`questions`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `snap2ask`.`questions` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `student_id` INT(11) NOT NULL,
+  `description` VARCHAR(512) NOT NULL,
+  `category_id` INT(11) NOT NULL,
+  `subcategory_id` INT(11) NOT NULL,
+  `status` INT(11) NOT NULL DEFAULT '0',
+  `times_answered` INT(11) NOT NULL DEFAULT '0',
+  `image_url` VARCHAR(255) NOT NULL,
+  `image_thumbnail_url` VARCHAR(255) NOT NULL,
+  `date_created` DATETIME NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_subcategories_1_idx` (`category_id`),
-  CONSTRAINT `fk_subcategories_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  INDEX `fk_questions_categories_idx` (`category_id` ASC),
+  INDEX `fk_questions_answers1_idx` (`status` ASC),
+  INDEX `fk_questions_users1_idx` (`student_id` ASC),
+  INDEX `fk_questions_1_idx` (`subcategory_id` ASC),
+  CONSTRAINT `fk_questions_1`
+    FOREIGN KEY (`subcategory_id`)
+    REFERENCES `snap2ask`.`subcategories` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_questions_categories`
+    FOREIGN KEY (`category_id`)
+    REFERENCES `snap2ask`.`categories` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_questions_users1`
+    FOREIGN KEY (`student_id`)
+    REFERENCES `snap2ask`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 31
+DEFAULT CHARACTER SET = latin1;
 
---
--- Table structure for table `users`
---
 
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(100) NOT NULL,
-  `oauth_id` varchar(100) DEFAULT NULL,
-  `password` varchar(100) NOT NULL,
-  `salt` varchar(100) NOT NULL,
-  `balance` int(11) NOT NULL DEFAULT '10',
-  `is_tutor` tinyint(4) NOT NULL DEFAULT '0',
-  `is_admin` tinyint(4) NOT NULL DEFAULT '0',
-  `preferred_category_id` int(11) DEFAULT '1',
-  `date_created` datetime NOT NULL,
-  `authentication_mode` enum('custom','facebook','google') NOT NULL,
-  `first_name` varchar(45) NOT NULL,
-  `last_name` varchar(45) NOT NULL,
-  `rating` int(11) NOT NULL,
+-- -----------------------------------------------------
+-- Table `snap2ask`.`answers`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `snap2ask`.`answers` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `question_id` INT(11) NOT NULL,
+  `tutor_id` INT(11) NOT NULL,
+  `text` VARCHAR(512) NOT NULL,
+  `rating` SMALLINT(6) NULL DEFAULT NULL,
+  `status` VARCHAR(45) NOT NULL DEFAULT 'pending',
+  `date_created` DATETIME NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `email_UNIQUE` (`email`,`authentication_mode`),
-  KEY `fk_users_1_idx` (`preferred_category_id`),
-  CONSTRAINT `fk_users_1` FOREIGN KEY (`preferred_category_id`) REFERENCES `categories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+  INDEX `fk_answers_1_idx` (`question_id` ASC),
+  INDEX `fk_answers_2_idx` (`tutor_id` ASC),
+  CONSTRAINT `fk_answers_1`
+    FOREIGN KEY (`question_id`)
+    REFERENCES `snap2ask`.`questions` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_answers_2`
+    FOREIGN KEY (`tutor_id`)
+    REFERENCES `snap2ask`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 15
+DEFAULT CHARACTER SET = latin1;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-10-13  1:08:33
+-- -----------------------------------------------------
+-- Table `snap2ask`.`verified_categories`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `snap2ask`.`verified_categories` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `category_id` INT NOT NULL,
+  `is_preferred` TINYINT NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `index2` (`user_id` ASC, `category_id` ASC),
+  INDEX `fk_verified_categories_2_idx` (`category_id` ASC),
+  CONSTRAINT `fk_verified_categories_1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `snap2ask`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_verified_categories_2`
+    FOREIGN KEY (`category_id`)
+    REFERENCES `snap2ask`.`categories` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `snap2ask`.`verification_questions`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `snap2ask`.`verification_questions` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `category_id` INT NOT NULL,
+  `question` VARCHAR(255) NOT NULL,
+  `choice_1` VARCHAR(255) NOT NULL,
+  `choice_2` VARCHAR(255) NOT NULL,
+  `choice_3` VARCHAR(255) NOT NULL,
+  `choice_4` VARCHAR(255) NOT NULL,
+  `correct_choice` SMALLINT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_verification_questions_1_idx` (`category_id` ASC),
+  CONSTRAINT `fk_verification_questions_1`
+    FOREIGN KEY (`category_id`)
+    REFERENCES `snap2ask`.`categories` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
