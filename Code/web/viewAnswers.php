fff1fbdc8cce2f3e341a261d46a17599546a001b
Vipul Kohli <vkohli678@yahoo.com>
2013-11-10 15:43:24 -0600

    viewAnswers.php functional but SLOW (no sorting for now)

--- Code/web/viewAnswers.php
+++ Code/web/viewAnswers.php
@@ -8,7 +8,7 @@ session_start();
 define('inc_file', TRUE);
 
 if (!isset($_SESSION['user_id'])) {
-	// The user is not logged in
+    // The user is not logged in
 	header('Location: index.php');
 	exit;
 }
@@ -20,24 +20,6 @@ $responseObj = getUserInfo(true);
 
 ?>
 
-<?php
-
-function stars($index)
-{
-   echo '<span class="rating">Rating: </span>
-   <span class="stars">';
-   $numStars = rand(1,5);
-   for ($count = 0; $count < $numStars; $count++)
-        echo '&#9733';
-    echo '</span>';
-}
-            
-?>
-
-   
-
-
-
 <!DOCTYPE html>
 <html>
 
@@ -47,7 +29,7 @@ function stars($index)
 	<link rel="shortcut icon" type="image/x-icon" href="res/favicon.ico">
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script src="js/validateBalance.js" type="text/javascript"></script>
-	<link rel="stylesheet" type="text/css" href="css/style.css">    
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 
 <body>
@@ -65,46 +47,11 @@ function stars($index)
 			</ul>
 		</div><!--end linksNav-->
 		
-		<div id="mainContent">
-    		<h1>MY ANSWERS</h1>
-            
-            <?php
-            
-            
-            for($i = 27; $i < 37; $i++)
-            {
-                $str = "https://snap2ask.s3.amazonaws.com/" . $i . ".jpeg";
-            ?>
-            	<div class="tutorViewAnswerContainer">
-	                
-	                <div class="tutorViewAnswersLeft">
-	                	<img class="tutorViewAnswerImage" alt="Question Image" src="<?php echo $str; ?>" />
-		                <label>Science</label>
-		                <label>Chemistry</label>
-		                <label>2013-11-07 04:07:20</label>
-	                </div>
-	
-	                <div class="tutorViewAnswersRight">	
-						<label>Question</label>
-						<p>
-						Lorem ipsum dolor sit amet.
-						</p>
-						
-						<label>Answer</label>
-						<p>
-						Lorem ipsum dolor sit amet.
-						</p>
-					</div>
-					
-				</div>
-				
-                
-            <?php
-            }
-            ?>
-			
-		</div>
-	</div>
+		<!--ans.php includes main content div-->
+		<!--remove style tag from ans.php-->
+		<?php include('ans.php'); ?>
+		
+	</div> <!--end content div-->
 	
 	<?php include('footer.php') ?>
 
