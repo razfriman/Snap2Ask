75c1e020f4dd60d7275d6383c823cab3baac18ab
Raymondd <rcmartin@smu.edu>
2013-11-18 16:40:01 -0600

    more valiation for balance

--- Code/web/js/validateBalance.js
+++ Code/web/js/validateBalance.js
@@ -2,17 +2,28 @@
 
 // we wait for the DOM to load
 $(document).ready(function () {
-	
-	//login form
-	//must be an email and password that is 8 characters in length
-	$('#withdrawSnapCashForm').validate({ 
+
+    var balance = parseInt($('div#cash h3').html().trim());
+                
+
+	//Checks the withdrawal amount
+	var validator = $('#withdrawSnapCashForm').validate({ 
 		rules: {
 			withdraw_amount: {
 				required: true,
+                digits: true,
+                is0: true,
 				min: 1,
-				digits: true
+                
 			}
 		},
+        messages:{
+            withdraw_amount:{
+                digits: "1Please use numbers",
+                required: "2Please enter an amount to withdraw",
+                min: "3You cannot withdraw negative SnapCash",
+            }
+        },
 		submitHandler: function(form) {
 			form.submit();
 		},
@@ -20,4 +31,18 @@ $(document).ready(function () {
 			error.insertBefore(element);
 		}
 	});
+
+
+
+    // this code sends is another warning to tell the user 0 is not a valid number
+    // to withdraww though it may be unnecessary
+    /*$('#withdrawSnapCashForm').validator.addMethod("is0", function(value, element) {
+        var withdraw = value;
+        if (withdraw === 0){
+            return false;
+        }
+
+        return true;
+
+    }, "You cannot withdraw 0 SnapCash");*/
 });
