e4759b67ef7f7158c05f8160c7ab999fcf092c5c
Vipul Kohli <vkohli678@yahoo.com>
2013-11-28 04:56:39 -0600

    Added JS and jQ for filtration by Subject & Rated/Not Rated
    Corresponding PHP and HTML revisions logged in next commit

--- Code/web/js/viewAnswers.js
+++ Code/web/js/viewAnswers.js
@@ -1,12 +1,11 @@
 $(document).ready(function () {
-
-$('#sortSelection').change(sortAnswers);
-$('#gobut').click(sortAnswers);
+    $('#sortSelection, #catlist').change(sortAnswers);
+    $('#gobut, #withrat').click(sortAnswers);
 });
 
 function sortAnswers() {
 
-	var index = $('#sortSelection')[0].selectedIndex;
+    var index = $('#sortSelection')[0].selectedIndex;
 		
 	switch(index) {
 		case 0:
@@ -29,27 +28,55 @@ function sortAnswers() {
 			break;
 	}
 	startDate = document.sortform.startdate.value;
-    	endDate = document.sortform.enddate.value;
-    	isStart = (startDate.length === 10);
-    	isEnd = (endDate.length === 10);
-    	if (isEnd || isStart){
-        	$('.tutorViewAnswerContainer').hide();
-        	for (ind = 0; ind < $('.tutorViewAnswerContainer').length; ind++){
-            	            var node = $('.tutorViewAnswerContainer')[ind];
+    endDate = document.sortform.enddate.value;
+    cat = document.sortform.catlist.value;
+    isStart = (startDate.length === 10);
+    isEnd = (endDate.length === 10);
+    rated = document.sortform.withrat.checked;
+    $('.tutorViewAnswerContainer').hide();
+    if (isEnd || isStart){
+        for (ind = 0; ind < $('.tutorViewAnswerContainer').length; ind++){
+            node = $('.tutorViewAnswerContainer')[ind];
             var ansdate = $(node).attr('answerDate');
-            		if (isStart && isEnd && ansdate.localeCompare(startDate) > 0 && ansdate.localeCompare(endDate) < 0){
-                    		$(node).show();
-                    		continue;
-            		}
-            		if (!isEnd && isStart && ansdate.localeCompare(startDate) > 0){
-                    		$(node).show();
-                    		continue;
-            		}
-            		if (!isStart && isEnd && ansdate.localeCompare(endDate) < 0){
-                    		$(node).show();
-                    		continue;
-            		}
-        	}//end for loop
-    	}//end outer if
-
+            anscat = $(node).attr('cat');
+            rating = $(node).attr('rating');
+            if (cat !== "All" && cat !== anscat)
+                continue;
+            if (rated == true && rating < 1)
+                continue;
+            if (isStart && isEnd && ansdate.localeCompare(startDate) > 0 && ansdate.localeCompare(endDate) < 0){
+                $(node).show();
+                continue;
+            }
+            if (!isEnd && isStart && ansdate.localeCompare(startDate) > 0){
+                $(node).show();
+                continue;
+            }
+            if (!isStart && isEnd && ansdate.localeCompare(endDate) < 0){
+                $(node).show();
+                continue;
+            }
+        }//end for loop
+    }//end outer if
+    else if(cat !== "All"){
+        for (ind = 0; ind < $('.tutorViewAnswerContainer').length; ind++){
+            node = $('.tutorViewAnswerContainer')[ind];
+            anscat = $(node).attr('cat');
+            rating = $(node).attr('rating');
+            if (cat !== "All" && cat !== anscat)
+                continue;
+            if (rated == true && rating < 1)
+                continue;
+            if (cat === anscat)
+                $(node).show();
+        }//end for loop
+    }
+    else{
+        for (ind = 0; ind < $('.tutorViewAnswerContainer').length; ind++){
+            node = $('.tutorViewAnswerContainer')[ind];
+            rating = $(node).attr('rating');
+            if (!rated || rating > 0)
+                $(node).show();
+        }//end for loop
+    }
 }//end sortAnswers function
